d83ab43df38ba175b5f11ef6fa3fa55b23550579
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-10-03 07:13:20 +0100

    Created a person object with 2 properties

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 // My first JavaScript code
 console.log('Hello World');
 
+// Variables
 let name = 'Elozino';
 let age = 30;
 
 console.log(name);
+
+// Objects
+let person = {
+    name: 'Elozino Lopez',
+    age: 30
+}
+
+
+console.log(person);
