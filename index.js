797e62e9eb29819c0ad19532b2e40e54236d8243
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-10-04 14:13:16 +0100

    Made the code cleaner

--- index.js
+++ index.js
@@ -121,14 +121,18 @@ function isLandScape (width, height) {
 
 //Exercise 3
 function fizzBuzz (input) {
+    if (typeof input !== 'number')
+        return 'Not a number';
 
     if ((input % 3) && (input % 5 === 0))
         return 'FizzBuzz';
+
     if (input % 3 === 0) 
         return 'Fizz';
+    
     if (input % 5 === 0) 
         return 'Buzz';
 }
 
-const output = fizzBuzz(5);
+const output = fizzBuzz(15);
 console.log(output);
