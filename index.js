75e31c37686ce34c7d0247b200dbd80703f4674c
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-10-04 13:56:08 +0100

    Replaced the return statement with console.log

--- index.js
+++ index.js
@@ -121,9 +121,9 @@ function isLandScape (width, height) {
 
 //Exercise 3
 function fizzBuzz (input) {
-    if (input % 3 === 0) return 'Fizz';
-    else if (input % 5 === 0) return 'Buzz';
-    else if (input % 3 && input % 5 === 0) return 'FizzBuzz';
+    if (input % 3 === 0) console.log('Fizz');
+    else if (input % 5 === 0) console.log('Buzz');
+    else if (input % 3 && input % 5 === 0) console.log('FizzBuzz');
 }
 
 const output = fizzBuzz(15);
