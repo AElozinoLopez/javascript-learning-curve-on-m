2fb73116f8cd1ce111f6560ea14d54beccdd70a7
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-10-04 14:19:25 +0100

    COmpleted the input logic of the AND statement

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ function fizzBuzz (input) {
     if (typeof input !== 'number')
         return 'Not a number';
 
-    if ((input % 3) && (input % 5 === 0))
+    if ((input % 3 === 0) && (input % 5 === 0))
         return 'FizzBuzz';
 
     if (input % 3 === 0) 
