a83353c828b84eafde6682d656fd8bce839d350c
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-10-05 07:46:42 +0100

    Introduced an if statememt to the function to ensure typeof is string

--- index.js
+++ index.js
@@ -202,7 +202,8 @@ const movie = {
 
 function showProperties(obj) {
     for (let key in obj)
-        console.log(key);
+        if (typeof obj[key] === 'string')
+            console.log(key, obj[key]);
 }
 showProperties(movie);
 
