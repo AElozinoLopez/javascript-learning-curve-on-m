9e47f8d2d24c387c79346a6c4d75bf4c57da7095
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-10-06 06:28:57 +0100

    Added another if statement to confirm number is prime and print it out

--- index.js
+++ index.js
@@ -261,10 +261,13 @@ function showPrimes(limit) {
     for (number = 2; number <= limit; number++) {
 
         let isPrime = true;
-            for (let factor = 2; factor < number; factor ++) {
-                if (number % factor === 0)
+        for (let factor = 2; factor < number; factor ++) {
+            if (number % factor === 0) {
                 isPrime = false;
                 break;
-            }
+            }                
+        }
+        if (isPrime === true) console.log(number);
     }
 }
+
