a5b7478d61d9ff253e877b8bf54aae965850508b
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-10-09 14:30:46 +0100

    changed syntax

--- index.js
+++ index.js
@@ -512,6 +512,6 @@ function CreateAnAddress(address, city, zipCode) {
 }
 
 //Creating a new address with the Constructor Function
-const addressOfChinwe = new CreateAnAddress('Court 1', 'Port-Harcourt', 232 );
+let addressOfChinwe = new CreateAnAddress('Court 1', 'Port-Harcourt', 232 );
 
-console.log('Address of Chinwe is', CreateAnAddress);
+console.log('Address of Chinwe is', addressOfChinwe);
