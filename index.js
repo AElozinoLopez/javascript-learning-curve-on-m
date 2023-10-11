171c9c7792a9fc7ab0f194db38f1927a02821c76
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-10-11 06:56:17 +0100

    Created another obejct for .$

--- index.js
+++ index.js
@@ -593,7 +593,7 @@ console.log(post);
 const priceRange = [
  {label: '$', tooltip: 'Inexpensive', minPerPerson: 0, maxPerPerson: 10},
  {label: '$$', tooltip: 'Moderate', minPerPerson: 11, maxPerPerson: 20},
-
+ {label: '$$$', tooltip: 'Expensive', minPerPerson: 21, maxPerPerson: 50}
 ]
 
 
