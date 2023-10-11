ce0e4716283df7a438db1658cd02b79e5b9d34a9
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-10-11 06:37:51 +0100

    Initialized comments to and empty array

--- index.js
+++ index.js
@@ -577,7 +577,7 @@ function BlogPost(title, body, author) {  //The fewer the parameters the easier 
   this.body = body;
   this.author = author;
   this.views = 0; //When a post is created, the views is zero by default
-  this.comments = 0;  //Same goes for the comment
+  this.comments = [];  //Same goes for the comment
   this.isLive = false; //Upon creation, the post is not yet live
 }
 
