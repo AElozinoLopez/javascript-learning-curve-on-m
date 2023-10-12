7b66352bfc1f05cc6ef81e664c1ede8102dd8796
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-10-12 14:26:21 +0100

    Added og to the log stement

--- index.js
+++ index.js
@@ -789,6 +789,6 @@ console.log(theMemoCombined);
 //The sort method is used to sort arrays
 const applicants = [2, 4, 3, 1, 5];
 applicants.sort();
-console.l(applicants);
+console.log(applicants);
 
 
