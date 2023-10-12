ae942b184752096092be9c1e2bcae3a4854b9dd2
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-10-12 06:54:40 +0100

    Added another method2 to slice

--- index.js
+++ index.js
@@ -739,6 +739,7 @@ console.log(combined);
 
 //Slicing using the slice method
 const sliced = combined.slice(2, 4);  //This picks the element at index 2 and picks the element before the end index (4 in this case). [3, 4] is returned
+const slicedMethod2 = combined.slice(2); // This begins at index 2 and returns all the elements afterwards 
 console.log(sliced);
 
 
