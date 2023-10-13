7e0be59d5f14c0735a343c66575db5cf4994d6e1
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-10-13 08:16:30 +0100

    Added an array called prices2

--- index.js
+++ index.js
@@ -933,6 +933,8 @@ console.log(summ);
 
 //The above can be achieved using the reduce() method. What this does is to reduce all the elements of the array into a single value, just like summing them up.
 //That single value can be anything - a string, a number, an object, anything!
+const prices2 = [1, 2, -3, 5, 7];
+
 prices.reduce((accumulator, currentValue) => {
   return accumulator + currentValue;
 }, 0);
