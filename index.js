e6540dd4f94dcd2472ff3fb48bf1bbb5104e2d77
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-10-17 23:54:10 +0100

    Added comments on the use of reduce() method to check for number of occurrences

--- index.js
+++ index.js
@@ -1032,12 +1032,11 @@ console.log(move(toyNumbers, 0, 5));
 const numberList = [1, 2, 3, 4, 5];
 
 function countOccurrences(array, searchElement) {
-  let countOccur = 0;                 // Initialize to 0
-  for (let element of array)          // Iterating the array
-    if (element === searchElement)    // Comparison to know how many times searcElement occurred in element
-      countOccur++;                   // Increment countOccur
-  return countOccur;                // Return countOccur
+  let countOccur = 0;                 // Initialize to 0                                                     || return array.reduce((accumulator, current) => {
+  for (let element of array)          // Iterating the array                                                 || const countOccur = (current === searchElement) ? 1 : 0; 
+    if (element === searchElement)    // Comparison to know how many times searchElement occurred in element || console.log(accumulator, current, searchElement);
+      countOccur++;                   // Increment countOccur                                                || accumulator + occurrence;
+  return countOccur;                // Return countOccur                                                     ||    }, 0);
 }
-
 console.log(countOccurrences(numberList, 2));
 
